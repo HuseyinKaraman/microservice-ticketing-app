a336ea734f85bafbc1ef7bf2222940d1f26f6089
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-03 20:50:22 +0300

    adding cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {json} from 'body-parser';
+import cookieSeesion from 'cookie-session';
 import 'express-async-errors';
 import { routes } from './routes';
 import { globalErrorHandler } from './middlewares/error-handler';
@@ -9,7 +10,15 @@ import start from './config';
 
 
 const app = express();
+
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSeesion({
+    signed: true,
+    secure: true
+  })
+)
 
 app.use("/api/users", routes);
 
