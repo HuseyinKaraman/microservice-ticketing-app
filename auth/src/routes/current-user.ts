c90db3d324e805f00cb72f1bfef8d1c12781f12e
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-09 01:08:34 +0300

    added current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,26 +1,10 @@
 import express, { Request, Response } from "express";
-import { AuthService } from "../services/auth";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/currentuser", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    res.status(401).send({ currentUser: null });
-    return;
-  }
-
-  try {
-    const payload = AuthService.verifyToken(req.session.jwt);
-    if (!payload) {
-      res.status(401).send({ currentUser: null });
-      return;
-    }
-    res.send({
-      currentUser: payload,
-    });
-  } catch (error) {
-    res.status(401).send({ currentUser: null });
-  }
+router.get("/currentuser", currentUser, (req: Request, res: Response) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
