ee69776a83b57d1ddfa088ed882957524de51331
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-10 01:04:15 +0300

    added test & seperate index file , config test

---
+++ auth/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import {json} from 'body-parser';
+import cookieSeesion from 'cookie-session';
+import 'express-async-errors';
+import { routes } from './routes';
+import { globalErrorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSeesion({
+    signed: false,
+    secure: true
+  })
+)
+
+app.use("/api/users", routes);
+
+app.all("*", (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(globalErrorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,7 @@
-import express from 'express';
-import {json} from 'body-parser';
-import cookieSeesion from 'cookie-session';
-import 'express-async-errors';
-import { routes } from './routes';
-import { globalErrorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import { connectDB } from './config/db';
-import start from './config';
-
-
-const app = express();
-
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSeesion({
-    signed: false,
-    secure: true
-  })
-)
-
-app.use("/api/users", routes);
-
-app.all("*", (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(globalErrorHandler);
+import { connectDB } from "./config/db";
+import start from "./config";
+import { app } from "./app";
 
 connectDB().then(() => {
   start(app);
-})
+});
