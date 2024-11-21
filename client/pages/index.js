cb4511481766d5d8e45dfeb5abafc2175fc4d2c2
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-22 00:45:05 +0300

    Updated LandingPage component to fetch current user data on server-side and client-side using axios. Added getInitialProps method to handle server-side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,26 @@
+import axios from 'axios';
 
-
-const Home = () => {
+const LandingPage = ({currentUser}) => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home - {currentUser && "You are signed in"}</h1>
         </div>
     )
 }
 
-export default Home;
+LandingPage.getInitialProps = async ({req}) => {
+    if (typeof window === 'undefined') {
+        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
+            headers: req.headers
+        }).catch((err) => {
+            console.log('error', err.response.data);
+        });
+        return data;
+    } else {
+        // We are on the browser
+        const {data} = await axios.get('/api/users/currentuser');
+        return data;
+    }
+}
+
+export default LandingPage;
