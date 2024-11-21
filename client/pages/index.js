b9f2e11da6323aa59840fb320b355852a7577419
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-22 00:58:18 +0300

    Implemented LandingPage component to display signed-in status based on currentUser prop, fetched using getInitialProps method.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,16 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
-const LandingPage = ({currentUser}) => {
-    return (
-        <div>
-            <h1>Home - {currentUser && "You are signed in"}</h1>
-        </div>
-    )
-}
+const LandingPage = ({ currentUser }) => {
+  return (
+      <h1>{currentUser ? "You are signed in" : "You are not signed in"}</h1>
+  );
+};
 
-LandingPage.getInitialProps = async ({req}) => {
-    if (typeof window === 'undefined') {
-        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
-            headers: req.headers
-        }).catch((err) => {
-            console.log('error', err.response.data);
-        });
-        return data;
-    } else {
-        // We are on the browser
-        const {data} = await axios.get('/api/users/currentuser');
-        return data;
-    }
-}
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
 
 export default LandingPage;
