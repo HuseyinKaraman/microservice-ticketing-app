7aa88efcf2b935effe3a612d38f69b025e9f5fa5
Huseyin Karaman <hsy.krmn@gmail.com>
2024-11-23 01:56:17 +0300

    bug fix & remove currentUser ssr from landing page move to _app.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,3 @@
-import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   return (
@@ -7,10 +6,8 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-
-  return data;
+  console.log("LANDING PAGE");
+  return {};
 };
 
 export default LandingPage;
